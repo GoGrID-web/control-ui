eece1a581663f84bf5ba439a598f6ca838d0e94f
Valdur Kana <valdur55@gmail.com>
2015-09-21 21:49:40 +0300

    Added Zone do model [Deliver #103833974]

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Door
+from .models import Zone, User, Door
 
+admin.site.register(Zone)
 admin.site.register(Door)
 admin.site.register(User)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Zone(models.Model):
+    name = models.CharField(max_length=10)
+    desc = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
 
 class Door(models.Model):
     d_uid = models.CharField(max_length=10)
     name = models.CharField(max_length=20)
+    zone = models.ManyToManyField(Zone)
 
     def __unicode__(self):
         return self.name
@@ -13,7 +20,7 @@ class User(models.Model):
     code = models.CharField(max_length=11)
     name = models.CharField(max_length=50)
     document = models.CharField(max_length=9)
-    door = models.ManyToManyField(Door)
+    zone = models.ManyToManyField(Zone)
 
     def __unicode__(self):
         return self.name
